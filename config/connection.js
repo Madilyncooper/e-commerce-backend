10d287ea3bdfd8d8a3a07c2dd7326a78c00758a9
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-05-19 14:51:36 -0700

    Cloned starting code, set up server connections, set up database connections and seeded database

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      dialectOptions: {
+      decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
