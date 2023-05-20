b9dc468c97fe075682e573396adcb7bcc0f8d7b9
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-05-20 11:19:30 -0700

    Added requirements for each model. Added connections to each model object

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,14 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
       host: '127.0.0.1',
+
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
       decimalNumbers: true,
       },
