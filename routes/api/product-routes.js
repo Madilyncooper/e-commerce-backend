6be4fd66564d33497ddeda063c8477ae62918569
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-05-21 14:18:08 -0700

    Added working GET api route for categories. Started to work on GET api route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,30 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+      attributes: {
+        include: [
+          [
+            sequelize.literal(
+              '()'
+            )
+          ]
+        ]
+      }
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
